c81167f8230d55ca17f8c43911d26f93010f5070
Heezol06 <shahriarrahmanheezol@gmail.com>
2022-06-04 22:20:34 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -3,11 +3,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
-      <h1>This is app js</h1>
-=======
       <h1>hello from heezol</h1>
->>>>>>> 5c4bc9148e21a45617b43e85e5e6a0a8a4d688c4
+      <h2>bal amar</h2>
     </div>
   );
 }
